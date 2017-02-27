c5172ae237a483468bcd0c782064834dccf7a0ce
phf <phf@atm-consulting.fr>
2017-02-27 17:31:02 +0100

    Fix creation dictionnaire

--- sql/llx_c_mission_order_reason.sql
+++ sql/llx_c_mission_order_reason.sql
@@ -3,5 +3,5 @@ CREATE TABLE llx_c_mission_order_reason (
     code     varchar(25) NOT NULL,
     label    varchar(80) NOT NULL,
     active   tinyint DEFAULT 1 NOT NULL,
-    entity   integer  DEFAULT 1 NOT NULL,
+    entity   integer  DEFAULT 1 NOT NULL
 )ENGINE=innodb;
